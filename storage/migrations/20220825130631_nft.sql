fc181da569c37ad4541930cc15b620cc2cfdcead
Максим Золотухин <maks@MacBook-Air-Maksim.local>
2022-08-30 14:01:58 +0300

    add verified, update migrations

--- storage/migrations/20220825130631_nft.sql
+++ storage/migrations/20220825130631_nft.sql
@@ -2,9 +2,21 @@
 CREATE DOMAIN t_address AS 
    varchar(67) NOT NULL;
 
-CREATE TABLE nft (
+CREATE TABLE nft_collection (
     address t_address PRIMARY KEY,
     owner t_address,
+    name text,
+    description text,
+    created timestamp NOT NULL,
+    updated timestamp,
+);
+
+CREATE TABLE nft (
+    address t_address PRIMARY KEY,
+    collection t_address NOT NULL,
+    owner t_address,
+    name text,
+    description text,
     created timestamp NOT NULL,
     updated timestamp,
 );
@@ -20,14 +32,18 @@ CREATE TABLE nft_auction (
     address t_address PRIMARY KEY,
     owner t_address PRIMARY KEY,
     nft t_address PRIMARY KEY,
+    price_token t_address NOT NULL,
+    start_price int64 NOT NULL,
+    max_bid int64 NOT NULL,
     created timestamp NOT NULL,
+    finished_at timestamp,
     updated timestamp,
 );
 
 CREATE TABLE nft_auction_bid (
     auction_id t_address PRIMARY KEY,
+    nft t_address PRIMARY KEY,
     from t_address PRIMARY KEY,
-    to t_address PRIMARY KEY,
     created timestamp NOT NULL,
     price_token t_address NOT NULL,
     price int64 NOT NULL,
@@ -35,10 +51,18 @@ CREATE TABLE nft_auction_bid (
 
 CREATE TABLE nft_forsale (
     nft t_address PRIMARY KEY,
-    from t_address PRIMARY KEY,
-    to t_address PRIMARY KEY,
     created timestamp NOT NULL,
     price_token t_address NOT NULL,
     price int64 NOT NULL,
 );
+
+CREATE TABLE nft_offer (
+    nft t_address PRIMARY KEY,
+    from t_address PRIMARY KEY,
+    created timestamp NOT NULL,
+    price_token t_address NOT NULL,
+    price int64 NOT NULL,
+    expired_at timestamp,
+);
+
 
