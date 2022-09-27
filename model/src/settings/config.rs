4ea668581ea4a1ca6173e3f881b0161ebaaccd99
Maxim Tugunchinov <tugunchinov.ma@phystech.edu>
2022-09-27 18:33:09 +0300

    change config

--- model/src/settings/config.rs
+++ model/src/settings/config.rs
@@ -1,11 +1,9 @@
 use serde::Deserialize;
-use std::{collections::HashMap, net::SocketAddr};
+use std::collections::HashMap;
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Config {
     pub database_url: String,
-    pub server_addr: SocketAddr,
-    pub healthcheck_addr: SocketAddr,
     pub database_max_connections: u32,
     pub kafka_topic: String,
     pub kafka_consumer_group: String,
