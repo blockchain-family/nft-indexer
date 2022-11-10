207968741d4222c7b4866bc4f239ed178486dfd5
tugunchinov <tugunchinov.maxim@gmail.com>
2022-11-10 22:06:33 +0300

    add api migrations

---
+++ storage/migrations/20221010090711_profile.sql
@@ -0,0 +1,10 @@
+create table profile(
+	address t_address primary key,
+    name varchar(100) not null,
+    bio text null,
+    image t_uri null,
+    site t_uri null,
+    email varchar(100) null,
+    twitter varchar(100) null,
+    created timestamp not null
+);
