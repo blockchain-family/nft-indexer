8279bc92aba62399f44e0ac7bbb43145efea2723
Maksim Greshnyakov <infiltrator53@gmail.com>
2023-04-26 12:39:25 +0200

    change db pool size debug

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -10,13 +10,13 @@ use sqlx::{
 };
 use std::{str::FromStr, time::Duration};
 
-pub async fn init_pg_pool(db_string: &str, pool_size: u32) -> Result<PgPool> {
+pub async fn init_pg_pool(db_string: &str, _pool_size: u32) -> Result<PgPool> {
     Ok(PgPoolOptions::new()
-        .max_connections(pool_size)
+        .max_connections(40)
         .connect_with(std::mem::take(
             PgConnectOptions::from_str(db_string)?
                 .log_statements(LevelFilter::Debug)
-                .log_slow_statements(LevelFilter::Debug, Duration::from_secs(10)),
+                .log_slow_statements(LevelFilter::Debug, Duration::from_secs(1)),
         ))
         .await?)
 }
