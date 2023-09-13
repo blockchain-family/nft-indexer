b93f944faceaa0ca328e2d8e6769a0543cc30949
Konstantin Snurnicyn <k.snurnicyn@esterna.eu>
2023-09-13 12:09:55 +0200

    MP-28 Fixed migration for nft_custom

--- indexer-repo/migrations/20230906112458_add_nft_collection_custom.sql
+++ indexer-repo/migrations/20230906112458_add_nft_collection_custom.sql
@@ -1,12 +1,10 @@
 create table nft_collection_custom(
     address t_address primary key,
-    owner t_address not null,
     updated timestamp not null,
     name text,
     description text,
     wallpaper t_uri,
     logo t_uri,
-    social jsonb
+    social jsonb,
+    FOREIGN KEY (address) REFERENCES nft_collection(address)
 );
-
-create index ix_nft_collection_custom_owner on nft_collection_custom using btree (owner);
