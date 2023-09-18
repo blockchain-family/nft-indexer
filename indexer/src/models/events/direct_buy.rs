ecece7c29da2ba90a3aa4e8239eba215c70ef49f
Maxim Tugunchinov <maxim.tugunchinov@bf.rocks>
2023-09-18 21:01:48 +0300

    update abi; add unique index for nft_collection_details

--- indexer/src/models/events/direct_buy.rs
+++ indexer/src/models/events/direct_buy.rs
@@ -1,7 +1,8 @@
 use nekoton_abi::{PackAbiPlain, UnpackAbiPlain};
 use serde::Serialize;
+use ton_block::MsgAddressInt;
 
-use crate::models::types::*;
+use crate::{models::types::*, utils::serialize_msg_address_int};
 
 #[derive(Clone, UnpackAbiPlain, PackAbiPlain, PartialEq, Eq, Debug, Serialize)]
 pub struct DirectBuyStateChanged {
@@ -11,4 +12,10 @@ pub struct DirectBuyStateChanged {
     pub to: u8,
     #[abi]
     pub value2: DirectBuyInfo,
+    #[abi]
+    #[serde(serialize_with = "serialize_msg_address_int")]
+    pub old_owner: MsgAddressInt,
+    #[abi]
+    #[serde(serialize_with = "serialize_msg_address_int")]
+    pub new_owner: MsgAddressInt,
 }
