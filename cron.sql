34901d98accd67bc74399a3a4a0491d33844dc31
Maksim Greshniakov <infiltrator53@gmail.com>
2023-10-07 09:44:22 +0200

    add migrate

--- cron.sql
+++ cron.sql
@@ -1,3 +1,4 @@
 -- after migration 20231004205345_nft_verified_extended_add_coalesce_index.sql
+select refresh_nft_verified_extended(true);
 select cron.schedule('execute refresh_nft_verified_extended', '*/6 * * * *',
                      'select refresh_nft_verified_extended(false);');
