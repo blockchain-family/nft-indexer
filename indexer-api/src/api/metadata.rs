2779d42bc5cd0361caac78fc09dcdb2b24396ead
Maksim Greshniakov <infiltrator53@gmail.com>
2023-10-10 16:55:28 +0200

    fixes

--- indexer-api/src/api/metadata.rs
+++ indexer-api/src/api/metadata.rs
@@ -11,6 +11,7 @@ pub struct RefreshMetadataParams {
     nft: Option<String>,
     #[opg(string)]
     collection: String,
+    #[opg(boolean)]
     only_collection_info: bool,
 }
 
@@ -21,59 +22,70 @@ pub async fn refresh_metadata_by_nft(
     meta_model_service: web::Data<MetadataModelService>,
 ) -> HttpResponse {
     let result = match path.0.nft {
-        None => {
-            if let Err(e) = data_reader::update_collections_meta(
-                &path.0.collection,
-                &meta_model_service,
-                &meta_jrpc_service,
-            )
-            .await
-            {
-                Err(e)
-            } else {
-                if !path.only_collection_info {
-                    match meta_model_service
-                        .get_nfts_by_collection(&path.0.collection)
-                        .await
-                    {
-                        Ok(nfts) => {
-                            let mut result = Ok(());
-
-                            for nft in nfts {
-                                if let Err(e) = data_reader::update_nft_meta(
-                                    &NftAddressData {
-                                        nft,
-                                        collection: path.0.collection.clone(),
-                                    },
-                                    &meta_model_service,
-                                    &meta_jrpc_service,
-                                )
-                                    .await
-                                {
-                                    result = Err(e);
-                                    break;
-                                }
-                            }
-
-                            result
-                        }
-                        Err(e) => Err(e),
-                    }
-                }
-            }
-        }
+        None => update_collection_metadata(&path, &meta_model_service, &meta_jrpc_service).await,
         Some(nft) => {
-            data_reader::update_nft_meta(
-                &NftAddressData {
-                    nft,
-                    collection: path.0.collection,
-                },
+            update_nft_metadata(
+                nft,
+                &path.0.collection,
                 &meta_model_service,
                 &meta_jrpc_service,
             )
             .await
         }
     };
+
+    respond_to_result(result)
+}
+
+async fn update_collection_metadata(
+    path: &Json<RefreshMetadataParams>,
+    meta_model_service: &web::Data<MetadataModelService>,
+    meta_jrpc_service: &web::Data<MetadataJrpcService>,
+) -> anyhow::Result<()> {
+    data_reader::update_collections_meta(
+        &path.0.collection,
+        meta_model_service,
+        meta_jrpc_service,
+    )
+    .await?;
+
+    if !path.only_collection_info {
+        let nfts = meta_model_service
+            .get_nfts_by_collection(&path.0.collection)
+            .await?;
+
+        for nft in nfts {
+            update_nft_metadata(
+                nft,
+                &path.0.collection,
+                meta_model_service,
+                meta_jrpc_service,
+            )
+            .await?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn update_nft_metadata(
+    nft: String,
+    collection: &str,
+    meta_model_service: &web::Data<MetadataModelService>,
+    meta_jrpc_service: &web::Data<MetadataJrpcService>,
+) -> anyhow::Result<()> {
+    data_reader::update_nft_meta(
+        &NftAddressData {
+            nft,
+            collection: collection.to_string(),
+        },
+        meta_model_service,
+        meta_jrpc_service,
+    )
+    .await
+}
+
+fn respond_to_result(result: anyhow::Result<()>) -> HttpResponse {
     match result {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(err) => {
